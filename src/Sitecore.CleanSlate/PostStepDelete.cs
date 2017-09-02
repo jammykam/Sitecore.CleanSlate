97956e1472599e83c36e719a181ad0c7cf837624
Kamruz Jaman <jammykam@gmail.com>
2017-09-02 13:39:18 -0400

    Initial commit of PostDeploy step and package to delete sample Sitecore Items

---
+++ src/Sitecore.CleanSlate/PostStepDelete.cs
@@ -0,0 +1,31 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Install.Framework;
+using Sitecore.Web;
+using System.Collections.Specialized;
+
+namespace Sitecore.CleanSlate
+{
+    public class PostStepDelete : IPostStep
+    {
+        public void Run(ITaskOutput output, NameValueCollection metaData)
+        {
+            var parameters = WebUtil.ParseUrlParameters(metaData["Attributes"], '|');
+
+            Database db = Factory.GetDatabase(parameters["database"]);
+            string[] items = parameters["items"].Split(',');
+
+            foreach (var i in items)
+            {
+                Item item = db.GetItem(i);
+                if (item != null)
+                {
+                    Log.Info("Post Install Delete : " + item.Paths.FullPath, this);
+                    item.Delete();
+                }
+            }
+        }
+    }
+}
